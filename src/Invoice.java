c0e2045a091a4128099d66f09de99f2de4684b92
SimranjeetSingh <simranjeet777000@gmail.com>
2026-06-23 21:55:15 +0530

    Building the structure of classes

--- src/Invoice.java
+++ src/Invoice.java
@@ -1,2 +1,27 @@
+import java.util.List;
+
 public class Invoice {
+    private Customer customerObj;
+    private List<Service> serviceList;
+    private double totalCost;
+
+    public Invoice(Customer customerObj, List<Service> serviceList, double totalCost) {
+        this.customerObj = customerObj;
+        this.serviceList = serviceList;
+        this.totalCost = totalCost;
+    }
+
+    public void addService(Service serviceObj)
+    {
+        serviceList.add(serviceObj);
+        totalCost += serviceObj.getServiceCost();
+    }
+
+    public void printInvoice()
+    {
+        System.out.println("----------Invoice-----------");
+        System.out.println("Customer Name: "+customerObj.getName()+" "+"Contact Number: "+customerObj.getContactNumber());
+        System.out.println("Car Model: "+customerObj.getCarObj().getCarModel()+" "+"Car Number: "+customerObj.getCarObj().getCarNumber());
+
+    }
 }
--- src/Service.java
+++ src/Service.java
@@ -1,2 +1,17 @@
 public class Service {
+    private String serviceType;
+    private double serviceCost;
+
+    public Service(String serviceType, double serviceCost) {
+        this.serviceType = serviceType;
+        this.serviceCost = serviceCost;
+    }
+
+    public String getServiceType() {
+        return serviceType;
+    }
+
+    public double getServiceCost() {
+        return serviceCost;
+    }
 }
