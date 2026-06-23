da0175226dd914743c80c17c0670eeb1c8d3178f
SimranjeetSingh <simranjeet777000@gmail.com>
2026-06-23 21:50:53 +0530

    Building the structure of Customer class

--- src/Customer.java
+++ src/Customer.java
@@ -1,2 +1,23 @@
 public class Customer {
+    private String customerName;
+    private String customerContactNumber;
+    private Car carObj;
+
+    public Customer(String customerName, String customerContactNumber, Car carObj) {
+        this.customerName = customerName;
+        this.customerContactNumber = customerContactNumber;
+        this.carObj = carObj;
+    }
+
+    public String getName() {
+        return customerName;
+    }
+
+    public String getContactNumber() {
+        return customerContactNumber;
+    }
+
+    public Car getCarObj() {
+        return carObj;
+    }
 }
