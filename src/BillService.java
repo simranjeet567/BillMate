7a1456554d66c2db1345611590aecb4377b6888e
SimranjeetSingh <simranjeet777000@gmail.com>
2026-06-24 13:56:48 +0530

    Building the structure of the entry point class i.e. BillService class

--- src/BillService.java
+++ src/BillService.java
@@ -1,2 +1,11 @@
+import java.util.*;
+
 public class BillService {
+    private Map<String, Customer> customers;
+    private List<Service> services;
+
+    public BillService(Map<String, Customer> customers, List<Service> services) {
+        this.customers = new HashMap<>();
+        this.services = new ArrayList<>();
+    }
 }
